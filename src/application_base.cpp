6ee5e2b2a48d40ff736dcb24313b87ad5bd6c639
Neel Raiyani <nraiyani@gmail.com>
2020-02-14 21:31:24 -0500

    Get supported extensions. what they are used for???

--- src/application_base.cpp
+++ src/application_base.cpp
@@ -1,5 +1,8 @@
 #include "application_base.hpp"
 
+#include <algorithm>
+#include <vector>
+
 using namespace vulkan_lessons;
 
 application_base::application_base(HWND hwnd)
@@ -18,10 +21,18 @@ void application_base::initialize_vulkan(HWND hwnd)
 {
     // initialize the vk::ApplicationInfo structure
     auto app_info = vk::ApplicationInfo(
-        "Vulkan Lessons", 1, 
-        "No Engine", 1, 
+        "Vulkan Lessons", 1, // Dummy name
+        "No Engine", 1,      // Dummny name
         VK_API_VERSION_1_2
     );
+
+    // checking extension support
+    auto extensions = vk::enumerateInstanceExtensionProperties();
+    std::sort(extensions.begin(), extensions.end(), 
+              [] (vk::ExtensionProperties const &a, vk::ExtensionProperties const &b)
+    {
+        return strcmp(a.extensionName, b.extensionName) < 0;
+    });
 
     // initialize the vk::InstanceCreateInfo
     auto instance_info = vk::InstanceCreateInfo({}, &app_info);
